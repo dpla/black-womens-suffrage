1db4e42214e894baf5759b24d08815506ea962e1
kcontreras01 <kcontreras01@gmail.com>
2020-07-29 14:12:17 -0400

    responsive-ish collection section

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -4,23 +4,37 @@ import RoundButton from "../shared/RoundButton"
 
 const HomePage = () => {
   return (
-    <div id={scss.main}>
-
-      <div id={scss.leftHeroDiv}>
-        <section className={scss.container} id={scss.leftContainer}>
-          <p className={scss.heroText}>Black women’s suffrage.</p>
-          <p className={scss.heroText}>Thousands of artifacts.</p>
-          <p className={scss.heroText}>Thousands of stories.</p>
-          <RoundButton text="LEARN MORE" icon={"../static/images/icon/button-arrow.svg"} id={scss.heroButton}/>
+    <>
+    <div className={scss.hero}>
+      <div className={scss.hero__left}>
+        <section className={`${scss.container} ${scss.container__left}`}>
+          <p className={scss.hero__text}>Black women’s suffrage.</p>
+          <p className={scss.hero__text}>Thousands of artifacts.</p>
+          <p className={scss.hero__text}>Thousands of stories.</p>
+          <RoundButton text="LEARN MORE" icon={"../static/images/icon/button-arrow.svg"} className={scss.hero__button}/>
         </section>
       </div>
-      
-
-      <section className={scss.container} id={scss.rightContainer}>
-
-      </section>
-
+      <div className={scss.hero__right}>
+        <div className={`${scss.container} ${scss.container__right}`}></div>
+      </div>   
     </div>
+
+    <div className={scss.collection}>
+      <div className={scss.collection__left}>
+        <h6>BLACK WOMEN’S SUFFRAGE PORTAL</h6>
+        <p className={scss.collection__text_large}>The Black Women’s Suffrage Digital Collection is a collaborative project to provide digital access to materials documenting the roles and experiences of Black Women in the Women’s Suffrage Movement and, more broadly, women’s rights, voting rights,
+        and civic activism between the 1850s and 1960.</p>
+        <p className={scss.collection__text_small}>The materials in this collection include photographs, correspondence, speeches, event programs, publications, oral histories, and other artifacts.</p>
+        <RoundButton text="SEARCH THE COLLECTION" icon={"../static/images/icon/button-arrow.svg"} className={scss.collection__button}/>
+      </div>
+
+      <div className={scss.collection__right}>
+        <div className={scss.collection__img}></div>
+      </div>     
+    </div>
+
+    
+    </>
   )
 };
 
