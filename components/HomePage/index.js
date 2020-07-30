a71465a5dc37267b638334aa7de65ac0510434f5
kcontreras01 <kcontreras01@gmail.com>
2020-07-30 14:06:53 -0400

    text and graphic added

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -20,7 +20,7 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className={scss.collection}>
+      <section className={scss.home__section_1}>
         <StyledTextWithButtons
           title="BLACK WOMEN’S SUFFRAGE PORTAL"
           subtitle="The Black Women’s Suffrage Digital Collection is a collaborative project to provide digital access to materials documenting the roles and experiences of Black Women in the Women’s Suffrage Movement and, more broadly, women’s rights, voting rights,
@@ -29,15 +29,13 @@ const HomePage = () => {
           buttonText1="SEARCH THE COLLECTION"
           buttonIcon1="../static/images/icon/button-arrow.svg"
         />
-        <div className={scss.collection__right}>
-          <div className={scss.collection__img}></div>
+        <div className={scss.home__section_1__right}>
+          <div className={scss.home__section_1__img}></div>
         </div>
-      </div>
+      </section>
 
-      <div className={scss.home__section_2}>
-        <div className={scss.section_2__div_left}>
-
-        </div>
+      <section className={scss.home__section_2}>
+        <div className={scss.section_2__div_left}></div>
         <div className={scss.section_2__div_right}>
           <StyledTextWithButtons
             title="ADDING SHADES OF CONTEXT"
@@ -48,9 +46,22 @@ const HomePage = () => {
             buttonIcon2="../static/images/icon/button-arrow.svg"
           />
         </div>
-      </div>
+      </section>
 
-
+      <section className={scss.home__section_3}>
+        <div className={scss.section_3__div_leftt}>
+          <StyledTextWithButtons
+            title="DIGITAL COLLECTION"
+            subtitle="Loreum ipsum"
+            text="By combining archival materials from DPLA’s network of over 4,000 institutions, newly digitized content and partnerships, the collection seeks to engage students, educators and researchers in exploration and dialogue around this important, yet overlooked chapter in our nation’s history."
+            buttonText1="SEARCH ALL COLLECTIONS"
+            buttonIcon1="../static/images/icon/button-arrow.svg"
+          />
+        </div>
+        <div className={scss.section_3__div_right}>
+          <img src="../../static/images/graphic/home/home-graphic-body-2.png"/>
+        </div>
+      </section>
     </>
   )
 };
