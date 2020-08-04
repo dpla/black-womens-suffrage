60d6b0cfdd16d75a841193961c62c5bf20e98496
kcontreras01 <kcontreras01@gmail.com>
2020-08-04 16:52:41 -0400

    footer created

--- components/MainLayout/components/Footer/index.js
+++ components/MainLayout/components/Footer/index.js
@@ -6,10 +6,8 @@ const Footer = () => {
   return (
     <footer className={scss.footer}>
 
-      <section className={scss.footer__section}>
-        <div id={scss.footer__logo}>
-          <img src="static/images/logo/dpla_bws-logo-white-footer.png" />
-        </div>
+      <section className={scss.footer__section_top}>
+        <img src="static/images/logo/dpla_bws-logo-white-footer.png" />
 
         <nav className={scss.footer__links}>
           <Link href="/about">
@@ -37,20 +35,18 @@ const Footer = () => {
           </Link>
         </nav>
 
-        <div id={scss.navLogo}>
-          <img src="../static/images/logo/dpla-logo-footer.png" />
-        </div>
+        <img src="../static/images/logo/dpla-logo-footer.png" />
       </section>
 
       <section className={scss.footer__divider}></section>
 
-      <section className={scss.footer__section}>
+      <section className={scss.footer__section_bottom}>
         <p className={scss.footer__p}>The copyright status of items in DPLA's Black Women's Suffrage Collection varies. DPLA links to a wide variety of different materials: many are in the public domain,
 while others are under rights restrictions but nonetheless publicly viewable. For individual rights information about an item, please check the “Rights” field in the metadata,
 or follow the link to the digital object on the content provider’s website for more information.</p>
 
         <nav className={scss.footer__links_small}>
-          <a href="http://dp.la/"> Visit DPLA</a>
+          <a href="http://dp.la/" className={scss.footer__a}> Visit DPLA</a>
 
           <Link href="/harmful-language-disclaimer">
             <a>Harmful Language Disclaimer</a>
