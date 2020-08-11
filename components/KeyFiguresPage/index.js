88eca38a1c613dabb8e5d32ae0d490b84f2bb50b
kcontreras01 <kcontreras01@gmail.com>
2020-08-11 16:15:17 -0400

    setup of key figures page

---
+++ components/KeyFiguresPage/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import scss from "../KeyFiguresPage/KeyFiguresPage.module.scss"
+
+const KeyFiguresPage = () => {
+  return (
+    <>
+    hi
+    </>
+  )
+}
+
+export default KeyFiguresPage
