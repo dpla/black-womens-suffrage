00811671105774fe895757d320bda521e119ce5a
kcontreras01 <kcontreras01@gmail.com>
2020-08-12 14:50:50 -0400

    adding missing components and pages folders

---
+++ pages/timeline/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MainLayout from "../../components/MainLayout"
+import TimelinePage from "../../components/TimelinePage"
+
+const Timeline = () => {
+  return (
+      <MainLayout className="main" role="main">
+        <TimelinePage />
+      </MainLayout>
+  )
+};
+
+export default Timeline;
---
+++ pages/topics/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MainLayout from "../../components/MainLayout"
+import TopicsPage from "../../components/TopicsPage"
+
+const Topics = () => {
+  return (
+      <MainLayout className="main" role="main">
+        <TopicsPage />
+      </MainLayout>
+  )
+};
+
+export default Topics;
