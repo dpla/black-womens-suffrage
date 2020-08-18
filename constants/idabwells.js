8ac6052fc74488a78d694ea5ca7e4b3b033e5dca
AudreyAltman <audreykaltman@gmail.com>
2020-08-18 16:30:10 -0400

    add idabwells.js to constants

---
+++ constants/idabwells.js
@@ -0,0 +1,279 @@
+export const idaBWells = [ 
+  {
+    "root" : "ibwells-0008-010-14",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-14.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-14.pdf"
+  }, {
+    "root" : "ibwells-0008-010-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-02.pdf"
+  }, {
+    "root" : "ibwells-0008-009-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-02.pdf"
+  }, {
+    "root" : "ibwells-0009-007",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-007.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-007.pdf"
+  }, {
+    "root" : "ibwells-0010-001-07",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-07.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-07.pdf"
+  }, {
+    "root" : "ibwells-0010-002-17",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-17.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-17.pdf"
+  }, {
+    "root" : "ibwells-0010-008",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-008.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-008.pdf"
+  }, {
+    "root" : "ibwells-0009-004-05",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-05.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-05.pdf"
+  }, {
+    "root" : "ibwells-0008-008-03",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-03.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-03.pdf"
+  }, {
+    "root" : "ibwells-0010-004-04",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-04.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-04.pdf"
+  }, {
+    "root" : "ibwells-0010-004-05",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-05.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-05.pdf"
+  }, {
+    "root" : "ibwells-0008-008-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-02.pdf"
+  }, {
+    "root" : "ibwells-0009-004-04",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-04.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-04.pdf"
+  }, {
+    "root" : "ibwells-0010-002-16",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-16.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-16.pdf"
+  }, {
+    "root" : "ibwells-0010-001-06",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-06.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-06.pdf"
+  }, {
+    "root" : "ibwells-0009-006",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-006.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-006.pdf"
+  }, {
+    "root" : "ibwells-0008-009-03",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-03.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-03.pdf"
+  }, {
+    "root" : "ibwells-0010-001-10",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-10.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-10.pdf"
+  }, {
+    "root" : "ibwells-0010-002-20",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-20.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-20.pdf"
+  }, {
+    "root" : "ibwells-0008-011-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-011-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-011-02.pdf"
+  }, {
+    "root" : "ibwells-0010-005",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-005.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-005.pdf"
+  }, {
+    "root" : "ibwells-0010-003-21",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-003-21.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-003-21.pdf"
+  }, {
+    "root" : "ibwells-0008-009-04",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-04.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-04.pdf"
+  }, {
+    "root" : "ibwells-0008-009-12",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-12.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-12.pdf"
+  }, {
+    "root" : "ibwells-0008-010-08",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-08.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-08.pdf"
+  }, {
+    "root" : "ibwells-0010-001-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-01.pdf"
+  }, {
+    "root" : "ibwells-0010-002-11",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-11.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-11.pdf"
+  }, {
+    "root" : "ibwells-0009-004-03",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-03.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-03.pdf"
+  }, {
+    "root" : "ibwells-0008-008-05",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-05.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-05.pdf"
+  }, {
+    "root" : "ibwells-0010-004-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-02.pdf"
+  }, {
+    "root" : "ibwells-0008-010-12",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-12.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-12.pdf"
+  }, {
+    "root" : "ibwells-0008-009-08",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-08.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-08.pdf"
+  }, {
+    "root" : "ibwells-0001-001-008",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0001-001-008.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0001-001-008.pdf"
+  }, {
+    "root" : "ibwells-0008-009-09",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-09.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-09.pdf"
+  }, {
+    "root" : "ibwells-0008-010-13",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-13.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-13.pdf"
+  }, {
+    "root" : "ibwells-0010-004-03",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-03.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-03.pdf"
+  }, {
+    "root" : "ibwells-0008-008-04",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-04.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-04.pdf"
+  }, {
+    "root" : "ibwells-0009-004-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-02.pdf"
+  }, {
+    "root" : "ibwells-0008-010-09",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-09.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-09.pdf"
+  }, {
+    "root" : "ibwells-0008-009-13",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-13.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-13.pdf"
+  }, {
+    "root" : "ibwells-0008-009-05",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-05.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-05.pdf"
+  }, {
+    "root" : "ibwells-0009-004-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-01.pdf"
+  }, {
+    "root" : "ibwells-0010-002-13",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-13.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-13.pdf"
+  }, {
+    "root" : "ibwells-0010-001-03",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-03.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-03.pdf"
+  }, {
+    "root" : "ibwells-0008-009-06",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-06.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-06.pdf"
+  }, {
+    "root" : "ibwells-0008-010-11",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-11.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-11.pdf"
+  }, {
+    "root" : "ibwells-0008-009-10-11",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-10-11.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-10-11.pdf"
+  }, {
+    "root" : "ibwells-0008-009-07",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-07.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-07.pdf"
+  }, {
+    "root" : "ibwells-0010-001-02",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-02.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-02.pdf"
+  }, {
+    "root" : "ibwells-0010-002-12",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-12.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-12.pdf"
+  }, {
+    "root" : "ibwells-0010-004-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-01.pdf"
+  }, {
+    "root" : "ibwells-0008-011-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-011-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-011-01.pdf"
+  }, {
+    "root" : "ibwells-0010-001-09",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-09.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-09.pdf"
+  }, {
+    "root" : "ibwells-0010-002-19",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-19.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-19.pdf"
+  }, {
+    "root" : "ibwells-0010-003-22",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-003-22.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-003-22.pdf"
+  }, {
+    "root" : "ibwells-0010-004-06",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-06.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-004-06.pdf"
+  }, {
+    "root" : "ibwells-0008-008-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-008-01.pdf"
+  }, {
+    "root" : "ibwells-0009-004-07",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-07.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-07.pdf"
+  }, {
+    "root" : "ibwells-0010-002-15",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-15.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-15.pdf"
+  }, {
+    "root" : "ibwells-0010-001-05",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-05.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-05.pdf"
+  }, {
+    "root" : "ibwells-0008-009-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-009-01.pdf"
+  }, {
+    "root" : "ibwells-0010-001-04",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-04.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-04.pdf"
+  }, {
+    "root" : "ibwells-0010-002-14",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-14.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-14.pdf"
+  }, {
+    "root" : "ibwells-0009-004-06",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-06.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-004-06.pdf"
+  }, {
+    "root" : "ibwells-0008-0012",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-0012.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-0012.pdf"
+  }, {
+    "root" : "ibwells-0009-008",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-008.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0009-008.pdf"
+  }, {
+    "root" : "ibwells-0010-002-18",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-18.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-002-18.pdf"
+  }, {
+    "root" : "ibwells-0010-001-08",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-08.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0010-001-08.pdf"
+  }, {
+    "root" : "ibwells-0008-010-01",
+    "json" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-01.json",
+    "pdf" : "https://dpla-bws.s3.amazonaws.com/IBW-20200818/ibwells-0008-010-01.pdf"
+  }
+]
