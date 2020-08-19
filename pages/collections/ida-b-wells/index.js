75882ecdd011b6b68383d245a9da199fb4900efc
AudreyAltman <audreykaltman@gmail.com>
2020-08-19 16:46:41 -0400

    titles

---
+++ pages/collections/ida-b-wells/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import MainLayout from "../../../components/MainLayout"
+import fs from 'fs'
+import path from 'path'
+
+function IdaBWells({ itemsString }) {
+  const items = JSON.parse(itemsString)
+
+  return (
+      <MainLayout className="main" role="main">
+        <section>
+          <ul>
+            { Object.keys(items).map((key) => (
+              <li>
+                <p>{ items[key]["title"].join(": ") }</p>
+              </li>
+            )) }
+          </ul>
+        </section>
+      </MainLayout>
+  )
+};
+
+export async function getStaticProps() {
+  const ibwDirectory = path.join(process.cwd(), 'constants')
+  const filePath = path.join(ibwDirectory, 'ida-b-wells.js')
+  const itemsString = fs.readFileSync(filePath, 'utf8')
+
+  return {
+    props: {
+      itemsString,
+    },
+  }
+}
+
+
+export default IdaBWells;
