85aa975440114f8edfad68d262991794f206dba9
AudreyAltman <audreykaltman@gmail.com>
2020-08-21 10:27:02 -0400

    add thumbails, component

--- pages/collections/ida-b-wells/index.js
+++ pages/collections/ida-b-wells/index.js
@@ -1,37 +1,38 @@
 import React from "react"
 import MainLayout from "../../../components/MainLayout"
+import IdaBWellsPage from "../../../components/IdaBWellsPage"
 import fs from 'fs'
 import path from 'path'
 
 function IdaBWells({ items }) {
-  // const items = JSON.parse(itemsString);
 
   return (
       <MainLayout className="main" role="main">
-        <section>
-          <ul>
-            { items.map((item) => (
-              <li>
-                <p>{ item.title }</p>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <IdaBWellsPage items={ items } />
       </MainLayout>
   )
 };
 
 export async function getStaticProps() {
+
   const ibwDirectory = path.join(process.cwd(), 'constants');
   const filePath = path.join(ibwDirectory, 'ida-b-wells.js');
   const itemsString = fs.readFileSync(filePath, 'utf8');
   const json = JSON.parse(itemsString);
 
   const items = Object.keys(json).map((key) => {
+
     const title = json[key]["title"].join(": ");
+    const creator = json[key]["creator"].join("; ");
+    const description = json[key]["description"].join(" ");
+
+    const thumb = "/static/thumbnails/ibw/" + key + ".jpg";
 
     return {
-      title: title
+      title: title,
+      creator: creator,
+      description: description,
+      thumb: thumb
     }
   });
 
