6de596cb3361852c43cfb2453e2d107d9d8efbe3
kcontreras01 <kcontreras01@gmail.com>
2020-08-21 10:02:51 -0400

    timeline tile created

--- components/TimelinePage/index.js
+++ components/TimelinePage/index.js
@@ -10,6 +10,30 @@ const TimelinePage = () => {
   return (
     <>
       <PageBanner title={title} text={text} graphic={graphic} />
+
+      <section className={`section__default ${scss.timeline}`}>
+
+        <div className={scss.timeline__tile}>
+          <div className={scss.tile__year}>
+            <p>1833</p>
+            <div className={scss.tile__line}></div>
+          </div>
+          <div className={scss.tile__p}>
+            <p>The Philadelphia Female Anti-Slavery Society is founded. Among its founding members are Charlotte Vandine Forten and several of her daughters. This was the first biracial organization of female abolitionists in the United States, and one of six abolitionist groups the Forten family helped to found and finance.</p>
+          </div>
+        </div>
+
+        <div className={scss.timeline__tile}>
+          <div className={scss.tile__year}>
+            <p>1833</p>
+            <div className={scss.tile__line}></div>
+          </div>
+          <div className={scss.tile__p}>
+            <p>The Philadelphia Female Anti-Slavery Society is founded. Among its founding members are Charlotte Vandine Forten and several of her daughters. This was the first biracial organization of female abolitionists in the United States, and one of six abolitionist groups the Forten family helped to found and finance.</p>
+          </div>
+        </div>
+
+      </section>
     </>
   )
 }
