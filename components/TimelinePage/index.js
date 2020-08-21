c4c3fb59c153f83732eedc63b18b36c58388ee3e
kcontreras01 <kcontreras01@gmail.com>
2020-08-21 09:27:38 -0400

    page banner added

--- components/TimelinePage/index.js
+++ components/TimelinePage/index.js
@@ -1,6 +1,17 @@
 import React from "react";
+import scss from "components/TimelinePage/TimelinePage.module.scss"
+import PageBanner from "components/shared/PageBanner"
 
-const TimelinePage = () =>
-    <div>THIS PAGE INTENTIONALLY LEFT BLANK</div>;
+const TimelinePage = () => {
+  const title = "TIMELINE",
+  text = "Celebrating Black women and the vote.",
+  graphic = "static/graphic/timeline-page/timeline-graphic-hero.png";
+
+  return (
+    <>
+      <PageBanner title={title} text={text} graphic={graphic} />
+    </>
+  )
+}
 
 export default TimelinePage;
