9797a29c758875fac33f9de19872373d787a6c5c
Michael Della Bitta <mdellabitta@gmail.com>
2020-08-24 13:54:45 -0400

    search bar wip

---
+++ components/shared/SearchBar/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { withRouter } from "next/router";
+import css from "./SearchBar.module.scss";
+
+class SearchBar extends React.Component {
+
+    render() {
+        const { searchQuery, router } = this.props;
+
+        return (
+            <form action="/search" className={css.searchBar}>
+                <input
+                    key={searchQuery}
+                    className={css.searchInput}
+                    name="q"
+                    type="search"
+                    placeholder="Search the collection"
+                    aria-label="Search the collection"
+                    autoComplete="off"
+                    defaultValue={router.}
+                />
+                <button type="submit" className={css.searchButton}>
+                    Search
+                </button>
+            </form>
+        );
+    }
+}
+
+export default withRouter(SearchBar);
+
