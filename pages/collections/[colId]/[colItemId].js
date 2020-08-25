2d17db3a8df10caa36a006036ca119b03fb329af
AudreyAltman <audreykaltman@gmail.com>
2020-08-25 16:05:50 -0400

    fix fallback

--- pages/collections/[colId]/[colItemId].js
+++ pages/collections/[colId]/[colItemId].js
@@ -56,7 +56,7 @@ export async function getStaticPaths() {
     }));
   });
 
-  return { paths, fallback: true }
+  return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
