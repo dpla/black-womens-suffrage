cacc7b63282775fc6d0f68817e2e25cb4a278c86
AudreyAltman <audreykaltman@gmail.com>
2020-08-26 12:18:00 -0400

    key figures index redirect

---
+++ pages/key-figures/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Router from "next/router"
+import MainLayout from "components/MainLayout"
+import KeyFiguresPage from "components/KeyFiguresPage"
+import fs from 'fs'
+import path from 'path'
+import { keyFigures } from "constants/key-figures"
+
+function KeyFiguresIndex({ figure }) {
+
+  React.useEffect(() => {
+    Router.replace("/key-figures", `/key-figures/${ figure.figId }`)
+  })
+
+  return (
+      <MainLayout
+        className="main"
+        role="main"
+        pageTitle={ figure.name }
+      >
+        <KeyFiguresPage figure={ figure } />
+      </MainLayout>
+  )
+
+};
+
+export async function getStaticProps({ params }) {
+
+  const figId = Object.keys(keyFigures)[0];
+  var figure = keyFigures[figId];
+  figure["figId"] = figId;
+
+  return {
+    props: {
+      figure: figure
+    }
+  }
+}
+
+export default KeyFiguresIndex;
