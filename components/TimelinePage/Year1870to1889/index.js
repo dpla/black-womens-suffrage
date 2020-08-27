e360086d6876cb9639568a01ea2585235ab2b513
AudreyAltman <audreykaltman@gmail.com>
2020-08-27 13:28:24 -0400

    time period text only

--- components/TimelinePage/Year1870to1889/index.js
+++ components/TimelinePage/Year1870to1889/index.js
@@ -13,23 +13,147 @@ const Year1870to1889 = () => {
 
       <div className={scss.timeline__tile}>
         <div className={scss.tile__year}>
-          <p>  YEAR  </p>
+          <p>1870</p>
           <div className={scss.tile__line}></div>
         </div>
         <div className={scss.tile__p}>
-          <p>   TEXT   </p>
-          <p>   TEXT   </p>
+          <p>The Fifteenth Amendment is passed, giving African-American men the right to vote.</p>
+          <p>Charlotte Rollin is elected secretary of the integrated South Carolina Woman's Rights Association and at that years convention, says, "We ask suffrage not as a favor, not as a privilege, but as a right based on the grounds that we are human beings, and as such entitled to all human rights." According to historian Rosalyn Terborg-Penn, Rollin's speech is the first by an African-American woman other than Sojourner Truth to be preserved by white suffrage leaders in their history of the suffrage movement. (Terborg-Penn, 45)</p>
         </div>
       </div>
 
       <div className={scss.timeline__tile}>
         <div className={scss.tile__year}>
-          <p>  YEAR  </p>
+          <p>1871</p>
           <div className={scss.tile__line}></div>
         </div>
         <div className={scss.tile__p}>
-          <p>   TEXT   </p>
-          <p>   TEXT   </p>
+          <p>Charlotte Rollin writes in an 1871 article in AWSA's Woman's Journal: "We ask suffrage not as a favor, not as a privilege, but as a right based on the grounds that we are human beings and as such entitled to all human rights."</p>
+          <p>Charlotte Rollin leads a meeting at the South Carolina state capitol to advocate for women's suffrage.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1871-72</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Mary Ann Shadd Cary unsuccessfully attempts to register to vote in Washington, D.C., challenging the language of the Fourteenth and Fifteenth Amendments. Using her legal training, Cary unsuccessfully argues that as a taxpayer in the District of Columbia, she should have the same rights as male taxpayers. (Terborg-Penn, 38,39)</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1872</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Sojourner Truth shows up to vote at a polling station in Battle Creek, Michigan, but is turned away.</p>
+          <p>White suffragist Virginia Minor and her husband Francis sue the St. Louis registrar for barring Virginia Minor from voting, arguing the Fourteenth Amendment has been misinterpreted. The U.S. Supreme Court eventually decides against the Minors. As a result of this case, suffragists realize they need to push for a new constitutional amendment rather than trying to challenge the interpretation of the Fourteenth Amendment.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1873</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Frances E.W. Harper delivers the closing speech at the AWSA convention in New York City, saying, "much as white women need the ballot, colored women need it more." Harper also called for more intersectional demands like equal rights and equal access to education for Black women, which, according to historian Rosalyn Terborg-Penn, was likely a radical position to take in a single-issue organization like AWSA (Terborg-Penn, 47).</p>
+          <p>Mrs. Beatty attends the inaugural convention of the NWSA-affiliated Oregon State Woman Suffrage Association, showing that Black women were active in the western women's suffrage movement (Terborg-Penn, 50).</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1874</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>The Woman's Christian Temperance Union (WCTU) is founded. Both Black and white women favor temperance because men's drinking led to increased incidents of domestic violence and to men spending the wages their families needed.The temperance and women's suffrage movements become allied, leading the liquor industry to become one of the suffragists' strongest opponents, as they believed that if women could vote, they'd outlaw liquor. Frances E.W. Harper was affiliated with the Philadelphia WCTU in the late 1870s and was appointed the national superintendent of the Department of Work Among Colored People, serving from 1883 to 1890. She was succeeded by Lucy Thurman of Michigan.</p>
+          <p>Mary Ann Shadd Cary addresses the House of Representatives Judiciary Committee, calling out the hypocrisy that although she paid equal taxes to any man in Washington, D.C., she did not have equal rights, as she is not permitted to vote.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1876</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Mary Ann Shadd Cary writes to NWSA, asking that the names of 94 Black women from Washington, D.C. be added to a centennial autograph book as signers of the Women's Declaration of Sentiments.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1877</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Frances E.W. Harper addresses the Association for the Advancement of Women (AAW) Woman's Congress. Her talk, entitled, "The Colored Women of America," sets out the issues that would be taken up by Mary Church Terrell and others in the NACW in 1890s (Collier-Thomas, 54).</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1878</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>The Woman Suffrage amendment, written by Susan B. Anthony, is raised in Congress by California Senator A.A. Sargent. Although it ultimately fails, it includes the language that some 40 years later forms the basis for the 19th Amendment.</p>
+          <p>Mary Ann Shadd Cary announces that Black women, determined to win suffrage, "would support whatever party would allow them their rights, be it Republican or Democratic."</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1879</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p> In Massachusetts, women are granted the right to vote in school board elections.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1880</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Mary Ann Shadd Cary founds the Colored Women's Franchise Association in Washington, D.C. to advocate for Black women's suffrage, political rights, expanded access to education and improved employment opportunities and working conditions. The group's tenure is short-lived, perhaps because of the radical nature of Cary's positions (Terborg-Penn, 84) or because she organized before Black women formed their own national organization, with the 1896 founding of the National Association of Colored Women (NACW).</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1881</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Gertrude Bustill Mossell writes an article entitled "Woman's Suffrage," in which she calls for readers to become better informed about the history of the suffrage movement and women's rights. The article is reprinted in the New York Freeman in 1885.</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1884</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>One year into her tenure as the leader of the WCTU, Frances E.W. Harper voices support for the temperance movement, saying "As a race, we have a right to be interested in the success of the temperance movement (Terborg-Penn, 85)."</p>
+        </div>
+      </div>
+
+      <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1887</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>The U.S. Senate votes against women's suffrage.</p>
         </div>
       </div>
 
--- components/TimelinePage/Year1960topresent/index.js
+++ components/TimelinePage/Year1960topresent/index.js
@@ -13,23 +13,63 @@ const Year1960topresent = () => {
 
       <div className={scss.timeline__tile}>
         <div className={scss.tile__year}>
-          <p>  YEAR  </p>
+          <p>1962</p>
           <div className={scss.tile__line}></div>
         </div>
         <div className={scss.tile__p}>
-          <p>   TEXT   </p>
-          <p>   TEXT   </p>
+          <p>Mississippi sharecropper Fannie Lou Hamer attempts to register to vote. She is evicted from her home and faces routine violence and threats. She is forced to move repeatedly, and though she is eventually able to register to vote successfully, she becomes the victim of repeated and serious violent harassment and intimidation from local whites. Hamer begins a citizenship education program in Sunflower County, Mississippi.</p>
         </div>
       </div>
 
-      <div className={scss.timeline__tile}>
+       <div className={scss.timeline__tile}>
         <div className={scss.tile__year}>
-          <p>  YEAR  </p>
+          <p>1963</p>
           <div className={scss.tile__line}></div>
         </div>
         <div className={scss.tile__p}>
-          <p>   TEXT   </p>
-          <p>   TEXT   </p>
+          <p>Hamer and several other women are arrested in Winona, Mississippi, while returning from a voter registration workshop in South Carolina, for entering the area of a bus station reserved for whites. All of the women, but especially Hamer, are savagely beaten while in the Winona jail.</p>
+          <p>Rev. Martin Luther King, Jr. leads the March on Washington for Jobs and Freedom and makes his famed "I Have a Dream" speech.</p>
+        </div>
+      </div>
+
+       <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1964</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Civil Rights Act of 1964 is passed, guaranteeing equal employment, limiting the use of voter literacy tests and establishing federal authority over the racial integration of public facilities.</p>
+        </div>
+      </div>
+
+       <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1965</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Armed police attack civil rights marchers on the Edmund Pettus Bridge in Montgomery, Alabama.</p>
+          <p>The Voting Rights Act of 1965 is passed, prohibiting state and local efforts to prevent Black men and women from exercising their right to vote as laid out in the 15th and 19th amendments. The bill represents a victory for Black women, who had been working for more than a century to win and then ensure the vote for Black women and men.</p>
+        </div>
+      </div>
+
+       <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>1972</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>Shirley Chisholm becomes the first Black major-party candidate to run for U.S. president. She is also the first woman to run for the Democratic nomination.</p>
+        </div>
+      </div>
+
+       <div className={scss.timeline__tile}>
+        <div className={scss.tile__year}>
+          <p>2013</p>
+          <div className={scss.tile__line}></div>
+        </div>
+        <div className={scss.tile__p}>
+          <p>The U.S. Supreme Court strikes down a key component of Voting Rights Act, allowing nine states with a history of racial discrimination to change election laws without federal approval.</p>
         </div>
       </div>
 
