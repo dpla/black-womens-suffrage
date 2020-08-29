fabeee864bd8335c866a40bf54d562c79c70113e
Michael Della Bitta <michael@dp.la>
2020-08-29 15:10:04 -0400

    Feature/partners page (#46)

--- components/PartnersPage/index.js
+++ components/PartnersPage/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import PageBanner from "components/shared/PageBanner"
+
+
 function PartnersPage() {
-    return (<div>THIS PAGE INTENTIONALLY LEFT BLANK</div>);
+    const title = "PARTNERS",
+        text = "Reconstructing visibility of Black womens' activism together.",
+        graphic = "/static/graphic/partners-page/partners-graphic-hero.png"
+
+
+
+    return (
+        <>
+            <PageBanner title={title} text={text} graphic={graphic}/>
+        </>
+    );
 }
 
 export default PartnersPage;
