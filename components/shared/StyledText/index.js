292a95b122a4d06e255d8d33d2902a702036bf8e
Kiara Contreras <kcontreras01@gmail.com>
2020-09-01 13:57:07 -0400

    DT-1990: about (#59)
    * about page complete

--- components/shared/StyledText/index.js
+++ components/shared/StyledText/index.js
@@ -1,17 +1,14 @@
 import React from "react"
 import scss from "../StyledText/StyledText.module.scss"
 
-const StyledText = ({ title, subtitle, text}) => {
+const StyledText = ({ title, subtitle, text }) => {
   return (
     <>
-      <section className={scss.container}>
-
-        {title && 
-          <h6 className={scss.title}>{title}</h6>
-        }
-        <p className={scss.section__text_large}>{subtitle}</p>
-        <p className={scss.section__text_small}>{text}</p>
-      </section>
+      {title &&
+        <h6 className={scss.title}>{title}</h6>
+      }
+      <p className={scss.section__text_large}>{subtitle}</p>
+      <p className={scss.section__text_small}>{text}</p>
     </>
   )
 };
