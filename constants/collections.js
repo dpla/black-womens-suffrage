21750dc5c5b915f65d2ee40fdc04cf00c22dc2db
AudreyAltman <audreykaltman@gmail.com>
2020-09-03 10:42:22 -0400

    larger ibw thumbs

--- constants/collections.js
+++ constants/collections.js
@@ -1,7 +1,7 @@
 export const collections = {
   "ida-b-wells": {
     "lived": "1862-1931",
-    "intro": "Ida B. Wells, (1862-1931) teacher, journalist and anti-lynching activist.", 
+    "intro": "Ida B. Wells, (1862-1931) teacher, journalist and anti-lynching activist. ", 
     "image": "/static/image/collections-page/collections-image-ida.png",
     "name": "Ida B. Wells Barnett Papers",
     "description": "Contains correspondence, manuscript of Crusade for Justice: the Autobiography of Ida B. Wells, diaries, copies of articles and speeches by Wells, articles, and accounts about Wells, newspaper clippings, and photographs.",
@@ -9,7 +9,7 @@ export const collections = {
   },
   "mary-church-terrell": {
     "lived": "1863-1954",
-    "intro": "Mary Church Terrell (1863-1954) educator, writer, lecturer, suffragist, and civil rights leader.",
+    "intro": "Mary Church Terrell (1863-1954) educator, writer, lecturer, suffragist, and civil rights leader. ",
     "image": "/static/image/collections-page/collections-image-mary.png",
     "name": "Mary Church Terrell Exhibit",
     "description": "Contains materials from Oberlin College Archives exhibit exploring Terrell’s life and work through four lenses: learning, labor, leadership, and legacy.",
@@ -17,7 +17,7 @@ export const collections = {
   },
   "charlotta-bass": {
     "lived": "1879/80-1969",
-    "intro": "Charlotta A. Bass Papers (1879/80-1969) journalist, community activist, and candidate for several elected offices.",
+    "intro": "Charlotta A. Bass Papers (1879/80-1969) journalist, community activist, and candidate for several elected offices. ",
     "image": "/static/image/collections-page/collections-image-charlotta.png",
     "name": "Charlotta Bass Papers",
     "description": "Contains articles, speeches, and campaign materials that document Bass’s political life and activism.",
