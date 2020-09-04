cf7de3db78120db84630c1e8a5805395eea14e97
AudreyAltman <audreykaltman@gmail.com>
2020-09-04 12:01:15 -0400

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
-import MainLayout from "../components/MainLayout"
-import HomePage from "../components/HomePage"
-import Head from "next/head";
+import MainLayout from "components/MainLayout"
+import HomePage from "components/HomePage"
+import BWSHead from "components/BWSHead";
 
 const Home = () => {
   return (
       <MainLayout className="main" role="main">
-          <Head>
-              <title>Black Women's Suffrage | DPLA</title>
-          </Head>
+          <BWSHead 
+          pageTitle="Black Women's Suffrage | DPLA"
+          pageDescription="The Black Women’s Suffrage Digital Collection is a collaborative project to provide digital access to materials documenting the roles and experiences of Black Women in the Women’s Suffrage Movement and, more broadly, women’s rights, voting rights, and civic activism between the 1850s and 1960."
+          pageImage="/static/graphic/home-page/home-graphic-body-1-mobile.png"
+          pageImageCaption="Images from the Black Women's Suffrage collection"
+          />
         <HomePage/>
       </MainLayout>
   )
