2788fe6a03de50b047188f6f9a8b125b2611228a
kcontreras01 <kcontreras01@gmail.com>
2020-09-09 12:23:51 -0400

    updated meta data

--- components/BWSHead/index.js
+++ components/BWSHead/index.js
@@ -24,33 +24,42 @@ class BWSHead extends React.Component {
           <title>{pageTitle}</title>
 
           <meta charSet="utf-8" />
-
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-
+          <meta name='application-name' content='BWS Collection' />
           <meta name="referrer" content="origin-when-cross-origin" />
-          <meta
-            name="description"
-            content={pageDescription || defaultDescription}
-          />
 
-          <meta name="og:site_name" content={defaultPageTitle} />
-          <meta name="og:title" content={pageTitle || defaultPageTitle} />
-          <meta name="og:type" content={seoType || "website"} />
-          <meta name="og:image" content={pageImage || defaultImageUrl} />
-          <meta
-            name="og:description"
-            content={pageDescription || defaultDescription}
-          />
-          
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@dpla" />
-          <meta name="twitter:creator" content="@dpla" />
-          <meta name="twitter:image" content={pageImage || defaultImageUrl} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='BWS Collection' />
+          <meta name='description' content={pageDescription || defaultDescription}/>
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          {/* <meta name='msapplication-config' content='/static/icons/browserconfig.xml' /> */}
+          <meta name='msapplication-TileColor' content='#4E4294' />
+          <meta name='msapplication-tap-highlight' content='no' />
+          <meta name='theme-color' content='#000000' />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+                    
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/static/manifest.json' />
+          <link rel='mask-icon' href='/static/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='shortcut icon' href='/static/favicons/favicon.ico' />
+              
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:url' content='https://yourdomain.com' />
+          <meta name='twitter:title' content='BWS Collection' />
+          <meta name='twitter:description' content={pageDescription || defaultDescription}/>
+          <meta name='twitter:image' content={pageImage || defaultImageUrl}/>
           {pageImageCaption &&
-            <meta name="twitter:image:alt" content={pageImageCaption} />}
+            <meta name="twitter:image:alt" content={pageImageCaption} />} 
+          <meta name='twitter:creator' content='@dpla' />
+          <meta property='og:type' content={seoType || "website"} />
+          <meta property='og:title' content={pageTitle || defaultPageTitle} />
+          <meta property='og:description' content={pageDescription || defaultDescription}/>
+          <meta property='og:site_name' content={defaultPageTitle} />
+          {/* <meta property='og:url' content='https://yourdomain.com' /> */}
+          <meta property='og:image' content={pageImage || defaultImageUrl} />
 
         </Head>
 
@@ -60,4 +69,3 @@ class BWSHead extends React.Component {
 }
 
 export default BWSHead;
-
