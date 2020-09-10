d0d72569eaf8c19e9e781d10125421f762e28e81
Michael Della Bitta <mdellabitta@gmail.com>
2020-09-10 16:26:45 -0400

    sitemap initial implementation.

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,6 +1,9 @@
 import { keyFigures } from "../../constants/key-figures";
+import {timelineOptions} from "../../constants/timeline-options";
+import * as fs from "fs";
 
 const absoluteLink = (path) => { return `https://blackwomenssuffrage.dp.la${path}` }
+
 const buildEntry = (link, date) => {
     return `<url>
     <loc>${link}</loc>
@@ -17,37 +20,43 @@ const sitemap = (req, res) => {
         '/about',
         '/timeline',
         '/collections',
+        '/collections/ida-b-wells',
         '/partners'
     ].map(absoluteLink);
 
-    const collectionsLinks = [];
-
-    const keyFiguresLinks = Object
-            .keys(keyFigures)
-            .map(keyFigure => `/key-figures/${keyFigure}`)
+    const keyFiguresLinks = Object.keys(keyFigures)
+        .map(keyFigure => `/key-figures/${keyFigure}`)
         .map(absoluteLink);
 
-    const timelineLinks = [];
+    const timelineLinks = timelineOptions
+        .map(range => `/timeline/${range}`)
+        .map(absoluteLink);
+
+    const ibwItems = Object.keys(
+        JSON.parse(
+            fs
+                .readFileSync('constants/ida-b-wells.js')
+                .toString('utf-8')
+        ))
+        .map(key => `/collections/ida-b-wells/${key}`)
+        .map(absoluteLink);
 
     const entries = []
         .concat(staticLinks)
-        .concat(collectionsLinks)
         .concat(keyFiguresLinks)
         .concat(timelineLinks)
+        .concat(ibwItems)
         .map((link) => buildEntry(link, date));
 
-    const entryString = "".concat(...entries)
-
-    console.log(JSON.stringify(entryString));
-
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${entryString}
-</urlset>`;
+    const sitemap =
+        `<?xml version="1.0" encoding="UTF-8"?>\n` +
+        `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
+        + "".concat(...entries)
+        + `\n</urlset>`;
 
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/xml')
+    res.setHeader('Content-Type', 'application/xml');
     res.end(sitemap);
-}
+};
 
 export default sitemap;
