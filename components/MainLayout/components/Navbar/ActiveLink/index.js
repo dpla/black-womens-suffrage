bf458a1ba8a587c0177838ab5e40a335b4fcd602
AudreyAltman <audreykaltman@gmail.com>
2020-09-10 14:12:02 -0400

    cleanup

--- components/MainLayout/components/Navbar/ActiveLink/index.js
+++ components/MainLayout/components/Navbar/ActiveLink/index.js
@@ -4,16 +4,12 @@ import Link from 'next/link'
 import React, { Children } from 'react'
 
 const ActiveLink = ({ children, activeClassName, ...props }) => {
-  const { asPath } = useRouter()
   const { pathname } = useRouter()
   const child = Children.only(children)
   const childClassName = child.props.className || ''
 
-  // pages/index.js will be matched via props.href
-  // pages/about.js will be matched via props.href
-  // pages/[slug].js will be matched via props.as
   const className =
-    pathname.split("/")[1] === props.href.split("/")[1] || asPath === props.as
+    pathname.split("/")[1] === props.href.split("/")[1]
       ? `${childClassName} ${activeClassName}`.trim()
       : childClassName
 
