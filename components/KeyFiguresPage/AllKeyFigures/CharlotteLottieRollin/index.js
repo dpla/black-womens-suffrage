09fca0d95a18e47b54264e166806004b640e5e97
AudreyAltman <audreykaltman@gmail.com>
2020-09-17 14:59:37 -0400

    rollin

--- components/KeyFiguresPage/AllKeyFigures/CharlotteLottieRollin/index.js
+++ components/KeyFiguresPage/AllKeyFigures/CharlotteLottieRollin/index.js
@@ -9,6 +9,8 @@ import BorderedTextBox from "components/KeyFiguresPage/BorderedTextBox";
 import Sources from "components/KeyFiguresPage/Sources";
 import KeyFiguresPagination from "components/KeyFiguresPage/KeyFiguresPagination";
 import IntroAndPortrait from "components/KeyFiguresPage/IntroAndPortrait";
+import SearchTag from "shared/SearchTag";
+import Link from "next/link";
 
 const CharlotteLottieRollin = () => {
 
@@ -18,13 +20,13 @@ const CharlotteLottieRollin = () => {
 
       <section>
         <SectionTitle title="An Anti-Slavery Family" />
-        <p>Charlotte Rollin, known as Lottie, was the second eldest of the well-known Rollin Sisters of South Carolina. Rollin's mixed-race parents were financially prosperous and Lottie and her sisters Frances Ann, Kate, Louisa and Florence Rollin all attended secondary school in the North and became early women's suffrage activists. </p>
+        <p>Charlotte Rollin, known as Lottie, was the second eldest of the well-known Rollin Sisters of South Carolina. Rollin's mixed-race parents were financially prosperous and Lottie and her sisters <SearchTag tag="blackwomensuffrage_frances_rollin" label="Frances Ann"/>, Kate, Louisa and Florence Rollin all attended secondary school in the North and became early women's suffrage activists. </p>
         <p>After the Civil War, Lottie and her sister Kate established a school for freedmen in Charleston. The family moved to Columbia, and Lottie went on to work as a clerk at the South Carolina State House. The sisters were influential in the politics of the Reconstruction-era South and held well-attended salons at the Columbia, South Carolina residence that the four younger sisters shared.</p>
       </section>
          
       <section>
         <SectionTitle title="Push for Progress in the Palmetto State" />
-        <p>In 1869, Rollin gave a speech on the floor of the South Carolina House of Representatives in favor of universal suffrage. The next year, she was the chair and founder of the South Carolina Woman's Rights Association, and in 1871, she led a meeting at the state capitol pushing for women's suffrage. A member of the American Woman Suffrage Association (AWSA) and the organizer of its South Carolina chapter, Rollin wrote in an 1871 article in the AWSA's Woman's Journal: "We ask suffrage not as a favor, not as a privilege, but as a right based on the grounds that we are human beings and as such entitled to all human rights." In 1872, Rollin worked alongside her sisters, as well as AWSA leader Lucy Stone, to push for an ultimately unsuccessful constitutional amendment granting women's suffrage in South Carolina.</p>
+        <p>In 1869, Rollin gave a speech on the floor of the South Carolina House of Representatives in favor of universal suffrage. The next year, she was the chair and founder of the South Carolina Woman's Rights Association, and in 1871, she led a meeting at the state capitol pushing for women's suffrage. A member of the <SearchTag tag="blackwomensuffrage_national_american_women_suffrage_association" label="American Woman Suffrage Association (AWSA)"/> and the organizer of its South Carolina chapter, Rollin wrote in an 1871 article in the AWSA's Woman's Journal: "We ask suffrage not as a favor, not as a privilege, but as a right based on the grounds that we are human beings and as such entitled to all human rights." In 1872, Rollin worked alongside her sisters, as well as AWSA leader <Link href="/search?q=lucy+stone"><a>Lucy Stone</a></Link>, to push for an ultimately unsuccessful constitutional amendment granting women's suffrage in South Carolina.</p>
       </section>
 
       <section>
