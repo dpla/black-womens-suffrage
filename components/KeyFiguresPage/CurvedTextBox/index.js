af5bab9db07340b3efb1a9dfd99930404e9d8274
AudreyAltman <audreykaltman@gmail.com>
2020-09-21 14:08:37 -0400

    ida wells

--- components/KeyFiguresPage/CurvedTextBox/index.js
+++ components/KeyFiguresPage/CurvedTextBox/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import scss from "../CurvedTextBox/CurvedTextBox.module.scss"
 
+// `text` should be enclosed in a <p></p> tag.
 const CurvedTextBox = ({text}) => {
   return (
     <section className={scss.curved_text_box__container}>
-      <p>{text}</p>
+      {text}
     </section>
   )
 }
