a92001dec9611885061e35b1e17f2a79c3725c59
kcontreras01 <kcontreras01@gmail.com>
2021-01-14 15:54:00 -0500

    CI job file

---
+++ constants/mary-church-terrell.js
@@ -0,0 +1,18 @@
+{
+  "mary-church-terrell" : {
+    "title" : [ ],
+    "creator" : [ "unknown" ],
+    "subject" : [ ],
+    "isPartOf" : [ "Mary Church Terrell" ],
+    "type" : [  ],
+    "format" : [  ],
+    "date" : [ ],
+    "identifier" : [  ],
+    "rights" : [  ],
+    "description" : [ ],
+    "spatial" : [  ],
+    "publisher" : [  ],
+    "language" : [ ],
+    "href" : ""
+  }
+}
