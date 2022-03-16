b9c18106e04cc00fc42ce5800a4736f8717e6d6d
Michael Della Bitta <mdellabitta@gmail.com>
2022-03-16 15:23:36 -0400

    removed service worker (#124)
    was causing bugs and is of limited use for a site that is mainly for directing users to media.

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,8 @@ import '../stylesheets/styles.scss'
 import '../stylesheets/utils.scss'
 import '../stylesheets/reset.scss'
 import 'react-dropdown/style.css'
-import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, [])
-
   return <Component {...pageProps} />
 }
 
