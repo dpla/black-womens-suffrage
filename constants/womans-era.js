abe1b1e14ae89837672665025ceb73b1f4d5fdc3
Michael Della Bitta <mdellabitta@gmail.com>
2022-03-21 11:10:49 -0400

    load-bearing empty file (#126)
    * load bearing empty file
    
    * changed futile string

---
+++ constants/womans-era.js
@@ -0,0 +1,18 @@
+{
+    "womans-era" : {
+    "title" : [ ],
+        "creator" : [ "unknown" ],
+        "subject" : [ ],
+        "isPartOf" : [ "Womans Era" ],
+        "type" : [  ],
+        "format" : [  ],
+        "date" : [ ],
+        "identifier" : [  ],
+        "rights" : [  ],
+        "description" : [ ],
+        "spatial" : [  ],
+        "publisher" : [  ],
+        "language" : [ ],
+        "href" : ""
+}
+}
