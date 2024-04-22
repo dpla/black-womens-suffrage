3ba37db24035a55d6d3728f8bcb8f782bd5a24ee
Michael Della Bitta <mdellabitta@tutanota.com>
2024-04-22 13:12:12 -0400

    Parameterized the API endpoint.

--- pages/api/dpla/items/index.js
+++ pages/api/dpla/items/index.js
@@ -1,23 +1,22 @@
 import httpProxyMiddleware from "next-http-proxy-middleware";
 
 const proxy = (req, res) => {
-
   var separator = req.url.indexOf("?") === -1 ? "?" : "&";
   var newPath = req.url.replace(
     /^\/api\/dpla(.*)$/,
     "/" +
-    (process.env.API_VERSION || "v2") + // to support absent v2 env variable
+      (process.env.API_VERSION || "v2") + // to support absent v2 env variable
       "$1" +
       separator +
       "api_key=" +
       process.env.API_KEY
   );
 
-  req.url = newPath
+  req.url = newPath;
 
-    return httpProxyMiddleware(req, res, {
-        target: 'https://api.dp.la'
-    });
-}
+  return httpProxyMiddleware(req, res, {
+    target: process.env.API_URL,
+  });
+};
 
 export default proxy;
