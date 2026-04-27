a9340ba2f49c1862762efb27062fbba986a77385
Kevin Payravi <kevinpayravi@gmail.com>
2026-04-27 18:00:04 -0400

    Revamp and apply styling to ErrorPage

--- components/ErrorPage/index.js
+++ components/ErrorPage/index.js
@@ -2,30 +2,25 @@ import React from "react";
 import Link from "next/link";
 import MainLayout from "components/MainLayout";
 import BWSHead from "components/BWSHead";
+import scss from "./ErrorPage.module.scss";
 
 const ErrorPage = ({ pageTitle, heading, body }) => (
   <MainLayout>
     <BWSHead pageTitle={pageTitle} />
-    <div className="sidebarAndContentWrapper">
-      <div className="row">
-        <div className="col-xs-12 col-md-offset-2 col-md-8">
-          <div className="content">
-            <h1>{heading}</h1>
-            <p>{body}</p>
-            <ul>
-              <li>
-                <Link href="/">Return to the home page</Link>
-              </li>
-              <li>
-                <Link href="/search">Search the collection</Link>
-              </li>
-              <li>
-                <Link href="/about">Learn about this project</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div className={`site-max-width container ${scss.error}`}>
+      <h1>{heading}</h1>
+      <p>{body}</p>
+      <ul>
+        <li>
+          <Link href="/">Return to the home page</Link>
+        </li>
+        <li>
+          <Link href="/search">Search the collection</Link>
+        </li>
+        <li>
+          <Link href="/about">Learn about this project</Link>
+        </li>
+      </ul>
     </div>
   </MainLayout>
 );
